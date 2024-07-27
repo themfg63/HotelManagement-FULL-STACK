2269e698258ca0e0e723fba2a2eae621ccfdfeb0
Muhammed Furkan GÜNEŞ <themfg.63@gmail.com>
2024-07-27 15:52:27 +0300

    project is created

---
+++ src/main/java/com/TheMFG/HotelManagement/model/User.java
@@ -0,0 +1,68 @@
+package com.TheMFG.HotelManagement.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Email adresi boş bırakılamaz")
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank(message = "Ad Soyad boş bırakılamaz")
+    private String name;
+
+    @NotBlank(message = "Telefon No boş bırakılamaz")
+    private String phoneNumber;
+
+    private String password;
+
+    private String role;
+
+    private List<Booking> bookings = new ArrayList<>();
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
