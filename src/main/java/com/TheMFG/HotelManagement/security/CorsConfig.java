5a82336dd50758f81e6d75bf8450023e601548cb
Muhammed Furkan GÜNEŞ <themfg.63@gmail.com>
2024-08-01 14:54:10 +0300

    Spring Security classes created

---
+++ src/main/java/com/TheMFG/HotelManagement/security/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.TheMFG.HotelManagement.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    /*
+    bu bean, uygulamanızın farklı kökenlerden gelen HTTP isteklerini güvenli bir şekilde nasıl ele alacağını tanımlar.
+     */
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry){
+                registry.addMapping("/**")
+                        .allowedMethods("GET","POST","PUT","DELETE")
+                        .allowedOrigins("*");
+            }
+        };
+    }
+}
