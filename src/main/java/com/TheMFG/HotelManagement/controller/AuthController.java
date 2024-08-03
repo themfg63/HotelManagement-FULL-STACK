95646565bb4e117e18a63966a2aa9f1c224159cf
Muhammed Furkan GÜNEŞ <themfg.63@gmail.com>
2024-08-03 16:28:44 +0300

    AuthController and UserController created

---
+++ src/main/java/com/TheMFG/HotelManagement/controller/AuthController.java
@@ -0,0 +1,31 @@
+package com.TheMFG.HotelManagement.controller;
+
+import com.TheMFG.HotelManagement.dto.Request.LoginRequest;
+import com.TheMFG.HotelManagement.dto.Response.Response;
+import com.TheMFG.HotelManagement.model.User;
+import com.TheMFG.HotelManagement.service.Interface.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<Response> register(@RequestBody User user){
+        Response response = userService.register(user);
+        return ResponseEntity.status(response.getStatusCode()).body(response    );
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Response> login(@RequestBody LoginRequest loginRequest){
+        Response response = userService.login(loginRequest);
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
+}
